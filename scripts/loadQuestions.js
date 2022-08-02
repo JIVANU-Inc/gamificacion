3fc9f3f6bf3772dd5380cee1ce1a2786af4fabc8
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2022-08-02 16:41:13 +0100

    Mostrar preguntas - En orden

--- scripts/loadQuestions.js
+++ scripts/loadQuestions.js
@@ -4,15 +4,44 @@ $(document).ready(function () {
         url: "./xml/cuestionario.xml",
         dataType: "xml",
         success: function (xml) {
-            //var xmlDoc = $.parseXML( xml );   <------------------this line
-            //if single item
-            //var person = $(xml).find("pregunta").text();
-
-            //but if it's multible items then loop
             $(xml)
-                .find("quiz")
+                .find("enunciado")
                 .each(function () {
-                    $("#questions").append("<li>" + $(this).text() + "</li>");
+                    let res = [];
+                    var enunciado = $(this).text();
+                    var respuesta = $(this)
+                        .next()
+                        .find("opcion")
+                        .each(function () {
+                            var opcion = $(this).text();
+                            var correcta = $(this).attr("select");
+                            var respuesta = {
+                                opcion: opcion,
+                                correcta: correcta,
+                            };
+                            res.push(respuesta);
+                        });
+                    $("#questions").append(
+                        "<li>" +
+                            enunciado +
+                            "<br><li>" +
+                            res[0].opcion +
+                            " -> " +
+                            res[0].correcta +
+                            "</li><li>" +
+                            res[1].opcion +
+                            " -> " +
+                            res[1].correcta +
+                            "</li><li>" +
+                            res[2].opcion +
+                            " -> " +
+                            res[2].correcta +
+                            "</li><li>" +
+                            res[3].opcion +
+                            " -> " +
+                            res[3].correcta +
+                            "</li></li><br>"
+                    );
                 });
         },
     });

d85681d370ae2a131d1e57e299349a9732e25c02
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2022-08-24 15:23:30 +0100

    game buttons v1

--- scripts/gameMenu.js
+++
@@ -1,9 +0,0 @@
-// Load game menu with ajax request
-async function loadGameMenu() {
-    const gameMenu = document.getElementById("gameMenu");
-    var gameMenu_content = await fetch("./views/gameMenu.html");
-    var gameMenu_content = await gameMenu_content.text();
-    gameMenu.innerHTML = gameMenu_content;
-}
-
-loadGameMenu();

7c0e60e4247085f6e08a139a1ca99783b3b41f60
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2022-09-11 16:16:02 +0100

    comienzo logica de juego

--- scripts/core.js
+++ scripts/core.js
@@ -44,50 +44,69 @@ getXML().then((data) => {
 
 CEXT.onInit = () => {
     // TODO: Add logic here
-    let pregunta1 = `<div class="quest-name row justify-content-center">
-    <div class="col-xxl-5 col-xl-5 col-lg-7 col-md-8" style="width: 700px">
-        <div class="section-title text-center mb-60">
-            <h3 class="mb-15">PREGUNTA 1:</h3>
-        </div>
-    </div>
-</div>
-<div class="quests row">
-    <div class="col-lg-6 col-md-6">
-        <p class="puntuacion mb-5"><strong>Puntuación:</strong></p>
-    </div>
-    <div class="col-lg-6 col-md-6">
-        <p class="time mb-5"><strong>Tiempo restante:</strong></p>
-    </div>
-    <div class="row">
-        <div class="col-lg-6 col-md-6">
-            <div class="single-feature quest1">
-                <div class="content">
-                    <p>Sección para descargar documentos de estudio</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6">
-            <div class="single-feature quest2">
-                <div class="content">
-                    <p>Pon a prueba tus conocimientos mientras juegas</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6">
-            <div class="single-feature quest3">
-                <div class="content">
-                    <p>Sección para descargar documentos de estudio</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-md-6">
-            <div class="single-feature quest4">
-                <div class="content">
-                    <p>Sección para descargar documentos de estudio</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>`;
-    play.innerHTML = pregunta1;
+    var partidaNum = 0;
+
+    CEXT.onInit = () => {
+        // Realizar las llamadas de inicialización del juego
+        let estado = CEXT.getGlobalStatus(); // true si había una partida guardada, false en caso contrario
+
+        if (estado) {
+            // Si hay una partida guardada, cargarla
+            partidaNum = CEXT.getNumAttempts();
+            CEXT.loadGame();
+        } else {
+            // Si no hay partida guardada, iniciar una nueva partida
+            CEXT.startNewAttempt();
+
+            // Cargar las preguntas del fichero XML
+
+            let pregunta = `<div class="quest-name row justify-content-center">
+                                <div class="col-xxl-5 col-xl-5 col-lg-7 col-md-8" style="width: 700px">
+                                    <div class="section-title text-center mb-60">
+                                        <h3 class="mb-15">PREGUNTA 1:</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="quests row">
+                                <div class="col-lg-6 col-md-6">
+                                    <p class="puntuacion mb-5"><strong>Puntuación:</strong></p>
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <p class="time mb-5"><strong>Tiempo restante:</strong></p>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="single-feature quest1">
+                                            <div class="content">
+                                                <p>Sección para descargar documentos de estudio</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="single-feature quest2">
+                                            <div class="content">
+                                                <p>Pon a prueba tus conocimientos mientras juegas</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="single-feature quest3">
+                                            <div class="content">
+                                                <p>Sección para descargar documentos de estudio</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="single-feature quest4">
+                                            <div class="content">
+                                                <p>Sección para descargar documentos de estudio</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+            play.innerHTML = pregunta;
+        }
+    }
+
 }
